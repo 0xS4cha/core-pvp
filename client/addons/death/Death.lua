79b769901f0b1177a6b8feba699c78faa363512e
Sxcha <88593319+0xS4cha@users.noreply.github.com>
2025-01-12 18:44:04 +0100

    Update Death.lua

--- client/addons/death/Death.lua
+++ client/addons/death/Death.lua
@@ -1,6 +1,6 @@
 local isDead = false
 local Token = nil
-
+local canRespawn = false
 TriggerEvent("core:RequestTokenAcces", "core", function(t)
     Token = t
 end)
@@ -13,7 +13,12 @@ end)
 
 function PlayerInComa(data)
     if isDead then
+        canRespawn = false
         SetNuiFocus(true, true)
+        Citizen.CreateThread(function()
+            Citizen.Wait(_CONFIG.RESPAWNTIME * 1000)
+            canRespawn = true
+        end)
         if data.killedByPlayer then
             local killerInformation = TriggerServerCallback("core:getKillerInformation", Token, data.killerServerId)
             while killerInformation == nil do Wait(100) end
@@ -31,12 +36,47 @@ function PlayerInComa(data)
             timeleft = _CONFIG.RESPAWNTIME,
             killedByPlayer = data.killedByPlayer
         })
+        TriggerScreenblurFadeIn(10)
     end
 end
+
+RegisterNUICallback('death:respawn', function(data, cb)
+    if canRespawn then
+        isDead = false
+        TriggerScreenblurFadeOut(10)
+        SetNuiFocus(false, false)
+        TriggerEvent('core:RevivePlayer')
+        _NUI.SendNUIMessage('showDeath', {
+            show = false,
+            timeleft = _CONFIG.RESPAWNTIME,
+            killedByPlayer = false
+        })
+    end
+end)
+
+RegisterNetEvent("core:RevivePlayer")
+AddEventHandler("core:RevivePlayer", function()
+    --enlever le screen du coma au mec ici
+    Death.GetAllDamagePed = {}
+    _NUI.SendNUIMessage('showDeath', {
+        show = false,
+        timeleft = _CONFIG.RESPAWNTIME,
+        killedByPlayer = false
+    })
+    TriggerScreenblurFadeOut(10)
+    local pos = _SAFEZONE.SafeZones[1].safezone.coords
+    SetEntityCoordsNoOffset(p:ped(), pos.x, pos.y, pos.z, false, false, false, true)
+    NetworkResurrectLocalPlayer(pos, 0.0, true, false)
+
+    p:setHealth(200)
+
+
+
+end)
+
 RegisterCommand('pistol', function()
     local playerPed = PlayerPedId() 
     local weaponHash = GetHashKey("WEAPON_PISTOL")
     GiveWeaponToPed(playerPed, weaponHash, 250, false, true)
     SetCurrentPedWeapon(playerPed, weaponHash, true)
-    print(GetHashKey('WEAPON_PISTOL'))
 end, false)
