17c81d6ea04cd24ef33af66452b6c76c9f5a3987
Sxcha <88593319+0xS4cha@users.noreply.github.com>
2025-01-26 17:36:26 +0100

    Update Main.lua

--- client/addons/richPresence/Main.lua
+++ client/addons/richPresence/Main.lua
@@ -4,15 +4,15 @@ Citizen.CreateThread(function()
     --while true do -- desactivé pour l'instant car on a rien a mêtre a jour 
         SetDiscordAppId(999247110392266773)
         SetDiscordRichPresenceAsset("ldo_logo-upscale")
-        SetDiscordRichPresenceAssetText("discord.gg/9TSgWRNqXZ")
+        SetDiscordRichPresenceAssetText("discord.gg/LDO-PVP")
 
-        SetDiscordRichPresenceAction(0, "Jouer", "https://discord.gg/9TSgWRNqXZ")
-        SetDiscordRichPresenceAction(1, "Discord", "https://discord.gg/9TSgWRNqXZ")
+        SetDiscordRichPresenceAction(0, "Jouer", "https://discord.gg/LDO-PVP")
+        SetDiscordRichPresenceAction(1, "Discord", "https://discord.gg/LDO-PVP")
         --Wait(60000)
     --end
 end)
 
 RegisterNetEvent("core:UpdateRichPresence")
 AddEventHandler("core:UpdateRichPresence", function (number)
-    SetRichPresence(_CONFIG.ServerName.." - "..number.. " joueurs connectés")
+    SetRichPresence(_CONFIG.ServerName.." - "..number.. " joueurs connectés - "..p:getPlayerName()..' ['..p:getId()..']')
 end)
