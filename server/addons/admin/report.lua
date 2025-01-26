e307c41b95bdef4b29efec4a2a0af99129602225
Sxcha <88593319+0xS4cha@users.noreply.github.com>
2025-01-26 23:46:19 +0100

    Update report.lua

--- server/addons/admin/report.lua
+++ server/addons/admin/report.lua
@@ -1,54 +1,81 @@
 
 local Reports = {} 
+local reportId = 0
 
-
-
-
-RegisterNetEvent("core:admin:SendReport")
-AddEventHandler("core:admin:SendReport", function(token, tableReport)
-    if CheckPlayerToken(source, token) then-- TODO REMETTRE
-        for k,v in pairs (Reports) do
-            if v["uniqueID"] == tableReport["uniqueID"] then
-                TriggerClientEvent('core:ShowNotification', source,  "Vous avez déjà un report en cours.")
-                return
-            end
-        end
-        LastReport = #Reports + 1
-        tableReport["time"] = os.date("%x %Hh %Mmin %Ss")
-        tableReport["alreadyTake"] = false
-        Reports[LastReport] = tableReport
-        if StaffInStaffMode ~= nil then
-            for k,v in pairs(StaffInStaffMode) do
-                TriggerClientEvent('core:ShowNotification', v,  "Nouveau report de " .. tableReport["name"] .. " ( " .. tableReport["id"] .. " / ".. tableReport["uniqueID"] .." )")
+RegisterCommand('report', function(source, args, rawCommand)
+    local source = source
+    if source ~= 0 then
+        local player = GetPlayer(source)
+        if player then
+            if not Reports[source] then
+                Reports[source] = {}
+                local data = {}
+                reportId += 1
+                data['id'] = reportId
+                data['playerName'] = player:getPlayerName()
+                data['tempId'] = source
+                data['uuid'] = player:getId()
+                data['reason'] = table.concat(args, " ")
+                data['time'] = os.date("%x %Hh %Mmin %Ss")
+                data['data'] = {
+                    id = player:getSource(), 
+                    name = player:getPlayerName(),
+                    permission = player:getPermission(),
+                }
+                data['taken'] = false
+                data['staff'] = ''
+                Reports[source] = data
+                if StaffInStaffMode ~= nil then
+                    for k,v in pairs(StaffInStaffMode) do
+                        TriggerClientEvent('core:ShowNotification', v,  "New report of ~b~<C>" .. data["playerName"] .. "</C>~s~ #"..data['id'])
+                    end
+                end
             end
         end
     end
-end)
+end, false)
 
-RegisterNetEvent("core:admin:claimReport")
-AddEventHandler('core:admin:claimReport', function(token, reportId)
-    if CheckPlayerToken(source, token) then -- TODO REMETTRE
-        if Reports[reportId] ~= nil then
-            local staff = GetPlayer(source)
-            Reports[reportId]["alreadyTake"] = true
-            Reports[reportId]["staff"] = staff:getPlayerName()
-            TriggerClientEvent('core:ShowNotification', Reports[reportId]["id"], '~r~STAFF~s~', '~italic~Supported report~italic~', "Your report has been taken care of by a member of the staff.")
-        end
-    end
-end)
 
 RegisterNetEvent("core:admin:closeReport")
-AddEventHandler('core:admin:closeReport', function(token, reportId)
-    if Reports[reportId] ~= nil then
-        local data = Reports[reportId]
-        table.remove(Reports, reportId)
-        TriggerClientEvent('core:ShowNotification', data["id"], '~r~STAFF~s~', '~italic~Report clotured~italic~', "Your report has been closed by a member of the staff.")
-
+AddEventHandler('core:admin:closeReport', function(token, reportId, time)
+    local source = source
+    if CheckPlayerToken(source, token) then
+        local player = GetPlayer(source)
+        if player:getPermission() >= _PERMISSION['ADMINMENU'] then
+            if Reports[reportId] ~= nil then
+                if Reports[reportId]['time'] == time then
+                    local old = Reports[reportId]
+                    Reports[reportId] = nil
+                    TriggerClientEvent('core:ShowNotification', reportId,  "Your report has been close")
+                    TriggerClientEvent('core:ShowNotification', source,  "You have been close the report #"..old['id'])
+                end
+           end
+        end
     end
 end)
 
 RegisterServerCallback("core:admin:GetAllReports", function(source, token)
     if CheckPlayerToken(source, token) then -- TODO REMETTRE
         return Reports
+    end
+end)
+
+RegisterServerCallback("core:admin:claimReport", function(source, token, reportId, time)
+    local source = source
+    if CheckPlayerToken(source, token) then
+        local player = GetPlayer(source)
+        if player:getPermission() >= _PERMISSION['ADMINMENU'] then
+            if Reports[reportId] ~= nil then
+                if Reports[reportId]['time'] == time then
+                    if not Reports[reportId]['taken'] then
+                        Reports[reportId]['taken'] = true
+                        Reports[reportId]['staff'] = player:getPlayerName()
+                        return true
+                    else
+                        return false
+                    end
+                end
+            end
+        end
     end
 end)
