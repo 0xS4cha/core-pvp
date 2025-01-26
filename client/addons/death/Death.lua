799dbbe9046c9b5dba8e0fe0962446f9c614e7f8
Sxcha <88593319+0xS4cha@users.noreply.github.com>
2025-01-26 01:28:23 +0100

    Update Death.lua

--- client/addons/death/Death.lua
+++ client/addons/death/Death.lua
@@ -90,10 +90,27 @@ AddEventHandler("core:RevivePlayer", function()
     NetworkResurrectLocalPlayer(pos, 0.0, true, false)
     TriggerServerEvent('core:death:RequestUnregister', Token)
     p:setHealth(200)
-
-
 end)
 
+RegisterNetEvent("core:StealPlayer")
+AddEventHandler("core:StealPlayer", function()
+
+    Death.GetAllDamagePed = {}
+    isDead = false
+    _NUI.SendNUIMessage('showDeath', {
+        show = false,
+        timeleft = _CONFIG.RESPAWNTIME,
+        killedByPlayer = false
+    })
+    SetNuiFocus(false, false)
+    ForceStopCarry()
+    TriggerScreenblurFadeOut(10)
+    local pos = GetEntityCoords(p:ped())
+    SetEntityCoordsNoOffset(p:ped(), pos.x, pos.y, pos.z, false, false, false, true)
+    NetworkResurrectLocalPlayer(pos, 0.0, true, false)
+    TriggerServerEvent('core:death:RequestUnregister', Token)
+    p:setHealth(200)
+end)
 
 RegisterCommand('revive', function()
     Death.GetAllDamagePed = {}
@@ -111,41 +128,4 @@ RegisterCommand('revive', function()
     NetworkResurrectLocalPlayer(pos, 0.0, true, false)
     TriggerServerEvent('core:death:RequestUnregister', Token)
     p:setHealth(200)
-end, false)
-RegisterCommand('pistol', function()
-    local playerPed = PlayerPedId() 
-    local weaponHash = GetHashKey("WEAPON_PISTOL")
-    GiveWeaponToPed(playerPed, weaponHash, 250, false, true)
-    SetCurrentPedWeapon(playerPed, weaponHash, true)
-end, false)
-
-RegisterCommand('pistol50', function()
-    local playerPed = PlayerPedId() 
-    local weaponHash = GetHashKey("WEAPON_PISTOL50")
-    GiveWeaponToPed(playerPed, weaponHash, 250, false, true)
-    SetCurrentPedWeapon(playerPed, weaponHash, true)
-end, false)
-
-
-RegisterCommand('smg', function()
-    local playerPed = PlayerPedId() 
-    local weaponHash = GetHashKey("WEAPON_SMG")
-    GiveWeaponToPed(playerPed, weaponHash, 250, false, true)
-    SetCurrentPedWeapon(playerPed, weaponHash, true)
-end, false)
-
-RegisterCommand('ak', function()
-    local playerPed = PlayerPedId() 
-    local weaponHash = GetHashKey("weapon_assaultrifle")
-    GiveWeaponToPed(playerPed, weaponHash, 250, false, true)
-    SetCurrentPedWeapon(playerPed, weaponHash, true)
-end, false)
-
-RegisterCommand('heal', function()
-    local playerPed = PlayerPedId() 
-    SetEntityHealth(playerPed, 200)
-end, false)
-
-RegisterCommand("armor", function()
-    SetPedArmour(PlayerPedId(), 100)
 end, false)
