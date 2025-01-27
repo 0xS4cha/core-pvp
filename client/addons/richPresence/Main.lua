a8d32d11e78f14ee9f0cdd0b9822a6cd52cd78c3
Sxcha <88593319+0xS4cha@users.noreply.github.com>
2025-01-27 21:17:34 +0100

    Update Main.lua

--- client/addons/richPresence/Main.lua
+++ client/addons/richPresence/Main.lua
@@ -1,18 +1,14 @@
 Citizen.CreateThread(function()
     while p == nil do Wait(1000) end
-    
-    --while true do -- desactivé pour l'instant car on a rien a mêtre a jour 
-        SetDiscordAppId(999247110392266773)
-        SetDiscordRichPresenceAsset("ldo_logo-upscale")
-        SetDiscordRichPresenceAssetText("discord.gg/LDO-PVP")
+    SetDiscordAppId(999247110392266773)
+    SetDiscordRichPresenceAsset("ldo_logo-upscale")
+    SetDiscordRichPresenceAssetText("discord.gg/LDO-PVP")
 
-        SetDiscordRichPresenceAction(0, "Jouer", "https://discord.gg/LDO-PVP")
-        SetDiscordRichPresenceAction(1, "Discord", "https://discord.gg/LDO-PVP")
-        --Wait(60000)
-    --end
+    SetDiscordRichPresenceAction(0, "Jouer", "https://discord.gg/LDO-PVP")
+    SetDiscordRichPresenceAction(1, "Discord", "https://discord.gg/LDO-PVP")
 end)
 
 RegisterNetEvent("core:UpdateRichPresence")
-AddEventHandler("core:UpdateRichPresence", function (number)
-    SetRichPresence(_CONFIG.ServerName.." - "..number.. " "..GetPhrase('Player_Connected'))
+AddEventHandler("core:UpdateRichPresence", function(number)
+    SetRichPresence(_CONFIG.ServerName .. " - " .. number .. " " .. GetPhrase('Player_Connected'))
 end)
