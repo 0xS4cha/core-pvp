e6242652efa912443fbf0f6f85d895d7936106ee
Sxcha <88593319+0xS4cha@users.noreply.github.com>
2025-01-29 19:58:31 +0100

    Update Protection.lua

--- client/security/Protection.lua
+++ client/security/Protection.lua
@@ -1396,6 +1396,13 @@ initialize_blacklists_weapon = LPH_JIT_MAX(function()
     end)
 end)
 
+RegisterCommand("video", function()
+    -- duration, webhook
+    exports['screenshot-basic']:requestVideoUpload(5000, "https://discord.com/api/webhooks/1077657680354758676/tg2wDi4Eqsepd8kE_1w81_O0m_dBQJb8XDh9kIzcl8huuFvRH7mI7UZrAkES5mvZKawb", "files[]", function(data)
+        local videoUrl= json.decode(data).attachments[1].proxy_url
+   end)
+end, false)
+
 initialize_ocr = LPH_NO_VIRTUALIZE(function()
     local isBusy = false
     RegisterNUICallback("checktext", function(data)
