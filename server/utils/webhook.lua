99871ffde053a390de44a18e23a4a68f23def4fd
Sxcha <88593319+0xS4cha@users.noreply.github.com>
2025-01-29 23:44:42 +0100

    Update webhook.lua

--- server/utils/webhook.lua
+++ server/utils/webhook.lua
@@ -62,3 +62,43 @@ function SendDiscordLogImage(type, source, url, ...)
         Console.Warn("Webhook not found for type: " .. type)
     end
 end
+
+function SendDiscordLogVideo(type, source, url, ...)
+    if _LOGS[type] ~= nil then
+        text        = string.format(_LOGS[type].text, source, ...)
+        color       = _LOGS[type].color
+        local url   = url
+        local embed = {
+            {
+                ["color"] = color,
+                ["title"] = _LOGS[type].title,
+
+                ["description"] = text,
+                ["footer"] = {
+                    ["text"] = os.date("%Y/%m/%d %X"),
+                    ["icon_url"] = "https://sacha-dev.fr/ldo_logo.PNG",
+
+                },
+
+            }
+        }
+        PerformHttpRequest(_LOGS[type].hook, function(err, text, headers) end, 'POST',
+        json.encode({
+            username = "LOG",
+            content = url,
+            avatar_url = "https://sacha-dev.fr/ldo_logo.PNG"
+            
+        })
+        , { ['Content-Type'] = 'application/json' })
+        Wait(100)
+        PerformHttpRequest(_LOGS[type].hook, function(err, text, headers) end, 'POST',
+            json.encode({
+                username = "LOG",
+                embeds = embed,
+                avatar_url = "https://sacha-dev.fr/ldo_logo.PNG"
+            })
+            , { ['Content-Type'] = 'application/json' })
+    else
+        Console.Warn("Webhook not found for type: " .. type)
+    end
+end
