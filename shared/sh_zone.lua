5c1493f3693f5a82fe7b2687b22ee262aec13085
Sxcha <88593319+0xS4cha@users.noreply.github.com>
2025-01-31 11:33:04 +0100

    New lobby

--- shared/sh_zone.lua
+++ shared/sh_zone.lua
@@ -7,43 +7,43 @@ _SAFEZONE.SafeZones = {
         Name = 'Los Santos',
         Image = 'lspd.png',
         vehicle = {
-            coords = vector3(-114.8243560791, -1605.9400634766, 31.772033691406),
-            heading = 320.3176574707
+            coords = vector3(397.50283813477, -373.13510131836, 46.807842254639),
+            heading = 204.55288696289
         },
         clothingStore = {
-            Pos = vector3(-137.83258056641, -1595.5024414062, 34.243640899658 - 1.02),
-            Heading = 310.88244628906,
+            Pos = vector3(381.67391967773, -367.69674682617, 46.830966949463 - 1.02),
+            Heading = 337.4089050293,
             Blip = { display = 73, colour = 32, size = 0.6, name = 'STORE_Clothing' },
 
         },
         safezone = {
-            coords = vector3(-120.50500488281, -1587.791015625, 34.218776702881),
+            coords = vector3(389.69708251953, -355.88461303711, 48.024364471436),
             radius = 100.0,
             radiusblip = 100.0
         },
         lobbySelector = {
-            Pos = vector3(-120.33229064941, -1574.6842041016, 34.176578521729 - 1.02),
-            Heading = 117.94702148438,
+            Pos = vector3(407.61431884766, -357.76815795898, 46.852043151855 - 1.02),
+            Heading = 92.950416564941,
             Blip = { display = 459, colour = 32, size = 0.6, name = 'STORE_LobbySelector' },
         },
         squadMenu = {
-            Pos = vector3(-134.40435791016, -1580.4940185547, 34.207954406738 - 1.02),
-            Heading = 232.22766113281,
+            Pos = vector3(407.09765625, -349.62802124023, 46.866138458252 - 1.02),
+            Heading = 138.7335357666,
             Blip = { display = 378, colour = 32, size = 0.6, name = 'SquandAndCrew' },
         },
         vehicleMenu = {
-            Pos = vector3(-123.18510437012, -1591.1706542969, 34.20779800415 - 1.02),
-            Heading = 68.537330627441,
+            Pos = vector3(397.20834350586, -344.94378662109, 46.861000061035 - 1.02),
+            Heading = 144.76686096191,
             Blip = { display = 225, colour = 32, size = 0.6, name = 'GaragePNJ' },
         },
         chestMenu = {
-            Pos = vector3(-118.70900726318, -1586.0874023438, 34.212963104248 - 1.02),
-            Heading = 48.500534057617,
+            Pos = vector3(389.15240478516, -369.53848266602, 46.843032836914 - 1.02),
+            Heading = 6.7340703010559,
             Blip = { display = 850, colour = 32, size = 0.6, name = 'chestMenu' },
         },
         shopMenu = {
-            Pos = vector3(-139.80618286133, -1587.5706787109, 34.243644714355 - 1.02),
-            Heading = 237.16999816895,
+            Pos = vector3(375.40411376953, -363.00637817383, 46.815231323242 - 1.02),
+            Heading = 271.91918945312,
             Blip = { display = 52, colour = 32, size = 0.6, name = 'shopMenu' },
         },
     },
