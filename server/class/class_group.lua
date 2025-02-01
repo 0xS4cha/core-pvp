825b5cb3fd740853b5dc67a247825c4bd3aa9ee5
Sxcha <88593319+0xS4cha@users.noreply.github.com>
2025-02-01 02:59:00 +0100

    Update class_group.lua

--- server/class/class_group.lua
+++ server/class/class_group.lua
@@ -5,12 +5,7 @@ local groupPermission = {
     ["removePlayer"] = false,
     ["changeRank"] = false,
     ["changePerm"] = false,
-    ["deleteCrew"] = false,
     ["deleteRank"] = false,
-    ["addVeh"] = false,
-    ["removeVeh"] = false,
-    ["addProperty"] = false,
-    ["removeProperty"] = false,
     ["changeCrewPerm"] = false,
 }
 
@@ -19,12 +14,7 @@ local bossDefaultPermission = {
     ["removePlayer"] = true,
     ["changeRank"] = true,
     ["changePerm"] = true,
-    ["deleteCrew"] = true,
     ["deleteRank"] = true,
-    ["addVeh"] = true,
-    ["removeVeh"] = true,
-    ["addProperty"] = true,
-    ["removeProperty"] = true,
     ["changeCrewPerm"] = true,
 }
 Group = {
@@ -73,6 +63,15 @@ Group = {
         return false
     end,
 
+    DoesGroupExistId = function(id)
+        for k, v in pairs(allGroup) do
+            if v.id == id then
+                return true
+            end
+        end
+        return false
+    end,
+
     getGroupId = function(name)
         local result = MySQL.Sync.fetchScalar("SELECT * FROM group_list WHERE group_list.name = @name", {
             ['@name'] = name
@@ -85,11 +84,12 @@ Group = {
     end,
 
     getGroupRankId = function(groupId, rank)
-        local result = MySQL.Sync.fetchScalar("SELECT * FROM group_rank WHERE group_rank.id_group= @groupId and `rank` = @rank"
+        local result = MySQL.Sync.fetchScalar(
+            "SELECT * FROM group_rank WHERE group_rank.id_group= @groupId and `rank` = @rank"
             , {
-            ['@groupId'] = groupId,
-            ['@rank'] = rank
-        })
+                ['@groupId'] = groupId,
+                ['@rank'] = rank
+            })
         if result then
             return result
         else
@@ -97,16 +97,20 @@ Group = {
         end
     end,
 
+
     CreateGroup = function(source, name, rgb, description, rank)
         local source = source
         if not Group.DoesGroupExist(name) then
-            local result = MySQL.Sync.insert("INSERT INTO group_list (`name`, `description`, `rgb`) VALUES (@name, @description,  @rgb)"
+            local id = 0
+            local result = MySQL.Sync.insert(
+                "INSERT INTO group_list (`name`, `description`, `rgb`) VALUES (@name, @description,  @rgb)"
                 , {
-                ['@name'] = name,
-                ['@rgb'] = json.encode(rgb),
-                ['@description'] = description
-            })
+                    ['@name'] = name,
+                    ['@rgb'] = json.encode(rgb),
+                    ['@description'] = description
+                })
             if result then
+                id = result[1].id_group
                 Console.Success("Group ^2" .. name .. "^7 created")
                 TriggerClientEvent("core:ShowNotification", source, "~b~Création du groupe en cours...")
                 allGroup[result] = {}
@@ -115,21 +119,25 @@ Group = {
                 allGroup[result].devise = description
                 allGroup[result].color = rgb
                 for k, v in pairs(rank) do
-                    MySQL.Sync.insert('INSERT INTO group_rank (name, rank, id_group, permission) VALUES(?, ?, ?, ?)',  {
+                    local perm = groupPermission
+                    if k == 1 then
+                        perm = bossDefaultPermission
+                    end
+                    MySQL.Sync.insert('INSERT INTO group_rank (name, rank, id_group, permission) VALUES(?, ?, ?, ?)', {
                         v,
                         k,
                         result,
-                        json.encode(groupPermission)
+                        json.encode(perm)
                     })
                 end
                 Wait(100)
-                MySQL.Sync.insert('INSERT INTO group_members (id_player, label, id_rank, id_group) VALUES(?, ?, ?, ?)',  {
+                MySQL.Sync.insert('INSERT INTO group_members (id_player, label, id_rank, id_group) VALUES(?, ?, ?, ?)', {
                     GetPlayer(source):getId(),
-                    GetPlayer(source):getFullName(),
+                    GetPlayer(source):getPlayerName(),
                     1,
                     result
                 })
-                return true
+                return id
             else
                 TriggerClientEvent("core:ShowNotification", source, "~r~Il y a eu une erreur, veuillez réessayer")
                 Console.Error("Error while creating crew" .. name)
@@ -142,13 +150,14 @@ Group = {
 
     createRank = function(groupName, name, rank)
         if Group.DoesGroupExist(groupName) then
-            local result = MySQL.Sync.insert("INSERT INTO group_rank (name, rank, id_group, permission) VALUES (?, ?, ?, ?)"
+            local result = MySQL.Sync.insert(
+                "INSERT INTO group_rank (name, rank, id_group, permission) VALUES (?, ?, ?, ?)"
                 , {
                     name,
                     rank,
                     Group.getCrewId(groupName),
                     json.encode(groupPermission),
-            })
+                })
             if result then
                 -- CorePrint("Rank ^2" .. name .. "^7 created")
                 return true
@@ -163,12 +172,13 @@ Group = {
 
     setPermRank = function(groupName, rank, perm)
         if Group.DoesGroupExist(groupName) then
-            local result = MySQL.Sync.execute("UPDATE group_rank SET perm = @perm WHERE id_group = @groupId AND `rank` = @rank"
+            local result = MySQL.Sync.execute(
+                "UPDATE group_rank SET perm = @perm WHERE id_group = @groupId AND `rank` = @rank"
                 , {
-                ['@groupId'] = Group.getCrewId(groupName),
-                ['@rank'] = tonumber(rank),
-                ['@perm'] = json.encode(perm),
-            })
+                    ['@groupId'] = Group.getCrewId(groupName),
+                    ['@rank'] = tonumber(rank),
+                    ['@perm'] = json.encode(perm),
+                })
             if result then
                 -- CorePrint("Perm set for rank ^2" .. rank .. "^7")
                 return true
@@ -180,11 +190,11 @@ Group = {
             return false
         end
     end,
-    
+
     deleteGroup = function(groupName)
         if Group.DoesGroupExist(groupName) then
             local result = MySQL.Sync.execute("DELETE FROM group_list WHERE name = @groupId"
-                , {
+            , {
                 ['@groupId'] = groupName,
             })
             if result then
@@ -206,7 +216,7 @@ Group = {
                 return false
             end
             local result = MySQL.Sync.execute("DELETE FROM group_rank WHERE group_id = @GroupId AND `rank` = @rank"
-                , {
+            , {
                 ['@GroupId'] = Group.getGroupId(groupName),
                 ['@rank'] = rank,
             })
@@ -222,29 +232,48 @@ Group = {
         end
     end,
 
-    addPlayerInGroup = function(playerId, groupId, rank)
+    addPlayerInGroup = function(source, groupId, rank)
         MySQL.Async.execute("INSERT INTO group_members (id_player, label, id_rank, id_group) VALUES (?, ?, ?, ?)"
-            , {
-                GetPlayer(source):getId(),
-                GetPlayer(source):getFullName(),
-                Group.getGroupRankId(groupId, rank),
-                groupId
-                
-            }, function(affectedRows)
-                Console.Success("Player added in group")
+        , {
+            GetPlayer(source):getId(),
+            GetPlayer(source):getPlayerName(),
+            Group.getGroupRankId(groupId, rank),
+            groupId
+
+        }, function(affectedRows)
+            Console.Success("Player added in group")
+        end)
+        return true
+    end,
+
+    joinGroup = function(source, groupId)
+        local result = MySQL.Sync.fetchAll(
+        'SELECT rank from group_rank WHERE id_group = @GroupId ORDER BY rank DESC LIMIT 1', {
+            ['@GroupId'] = groupId,
+        })
+        MySQL.Async.execute("INSERT INTO group_members (id_player, label, id_rank, id_group) VALUES (?, ?, ?, ?)"
+        , {
+            GetPlayer(source):getId(),
+            GetPlayer(source):getPlayerName(),
+            result[1]['rank'],
+            groupId
+
+        }, function(affectedRows)
+            Console.Success("Player added in group")
         end)
         return true
     end,
 
     changePlayerRankInGroup = function(playerId, groupId, rank)
-        MySQL.Async.execute("UPDATE group_members SET id_rank = @rankId WHERE id_group = @groupId AND id_player = @playerId"
+        MySQL.Async.execute(
+            "UPDATE group_members SET id_rank = @rankId WHERE id_group = @groupId AND id_player = @playerId"
             , {
                 ['@groupId'] = groupId,
                 ['@id_player'] = playerId,
                 ['@rankId'] = Group.getGroupRankId(groupId, rank)
             }, function(affectedRows)
                 Console.Success("Player rank changed in crew")
-        end)
+            end)
         return true
     end,
 
@@ -268,16 +297,69 @@ Group = {
     end,
 
     getPlayerGroup = function(playerId)
-        local result = MySQL.Sync.fetchAll("SELECT * FROM group_members LEFT JOIN group_list ON group_members.id_group = group_list.id_group WHERE group_members.id_player = @playerId", {
-            ['@playerId'] = playerId
-        })
+        local result = MySQL.Sync.fetchAll(
+            "SELECT * FROM group_members LEFT JOIN group_list ON group_members.id_group = group_list.id_group WHERE group_members.id_player = @playerId",
+            {
+                ['@playerId'] = playerId
+            })
         if result[1] ~= nil then
             return result[1].name, result[1].id_group
         else
             return "None"
         end
     end,
+    getPermssionCrewFromPlayer = function(playerId)
+        local result = MySQL.Sync.fetchAll(
+            "SELECT group_members.*, group_list.*, group_rank.permission FROM group_members LEFT JOIN group_list ON group_members.id_group = group_list.id_group LEFT JOIN group_rank ON group_members.id_rank = group_rank.rank AND group_members.id_group = group_rank.id_group WHERE group_members.id_player = @playerId ",
+            {
+                ['@playerId'] = playerId
+            })
+        if result[1] ~= nil then
+            return json.decode(result[1].permission)
+        else
+            return false
+        end
+    end,
 
+    getStats = function(groupId)
+        if Group.DoesGroupExistId(groupId) then
+            local result = MySQL.Sync.fetchAll("SELECT deaths, kills FROM group_list WHERE id_group = @GroupId"
+            , {
+                ['@GroupId'] = groupId,
+            })
+            if result[1] ~= nil then
+                return tonumber(result[1].kills), tonumber(result[1].deaths)
+            end
+        end
+    end,
+    getMembers = function(groupId)
+        if Group.DoesGroupExistId(groupId) then
+            local result = MySQL.Sync.fetchAll("SELECT * FROM group_members WHERE id_group = @GroupId"
+            , {
+                ['@GroupId'] = groupId,
+            })
+            if result ~= nil then
+                return result
+            end
+        end
+    end,
+    getRanksId = function(groupId)
+        if Group.DoesGroupExistId(groupId) then
+            local result = MySQL.Sync.fetchAll("SELECT * FROM group_rank WHERE id_group = @GroupId"
+            , {
+                ['@GroupId'] = groupId,
+            })
+            if result ~= nil then
+                local RanksId = {}
+                local RanksList = {}
+                for k, v in pairs(result) do
+                    RanksId[k] = {value = k, label = v.name}
+                    RanksList[k] = {name = v.name, permission = json.decode(v.permission), rank = v.rank, id_rank = v.id_rank, id_group = v.id_group}
+                end
+                return RanksList, RanksId
+            end
+        end
+    end,
     getAllGroupInfoByName = function(groupName)
         if Group.DoesGroupExist(groupName) then
             if allGroup[Group.getGroupId(groupName)] then
@@ -292,10 +374,11 @@ Group = {
 
     getAllGroupMembersBygroupName = function(groupName)
         if Group.DoesGroupExist(groupName) then
-            local result = MySQL.Sync.fetchAll("SELECT * FROM group_members LEFT JOIN players ON group_members.id_player = players.id LEFT JOIN group_rank ON group_members.id_rank = group_rank.id WHERE group_members.id_group = @groupId"
+            local result = MySQL.Sync.fetchAll(
+                "SELECT * FROM group_members LEFT JOIN players ON group_members.id_player = players.id LEFT JOIN group_rank ON group_members.id_rank = group_rank.id WHERE group_members.id_group = @groupId"
                 , {
-                ['@groupId'] = Group.getGroupId(groupName)
-            })
+                    ['@groupId'] = Group.getGroupId(groupName)
+                })
             if result ~= nil then
                 return result
             else
@@ -308,10 +391,11 @@ Group = {
 
     getAllGroupGradeBygroupName = function(groupName)
         if Group.DoesGroupExist(groupName) then
-            local result = MySQL.Sync.fetchAll("SELECT * FROM group_list LEFT JOIN group_rank ON group_list.id = group_rank.id_group WHERE group_list.id = @groupId"
+            local result = MySQL.Sync.fetchAll(
+                "SELECT * FROM group_list LEFT JOIN group_rank ON group_list.id = group_rank.id_group WHERE group_list.id = @groupId"
                 , {
-                ['@groupId'] = Group.getGroupId(groupName)
-            })
+                    ['@groupId'] = Group.getGroupId(groupName)
+                })
             if result ~= nil then
                 return result
             else
@@ -341,9 +425,9 @@ Group = {
     getAllGroupPropertyByName = function(groupName)
         if Group.DoesGroupExist(groupName) then
             local result = MySQL.Sync.fetchAll("SELECT * FROM property WHERE crew = @groupName"
-                , {
-                    ['@groupName'] = groupName
-                })
+            , {
+                ['@groupName'] = groupName
+            })
             if result ~= nil then
                 return result
             else
@@ -358,4 +442,3 @@ Group = {
 
 --crew.loadAllCrew()
 Group.newLoadAllGroup()
-
